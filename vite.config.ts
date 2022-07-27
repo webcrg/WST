613857d29d0af68b48e2cdc46412f6482a1cda16
webcrg <webcrg@yandex.ru>
2022-07-27 13:11:51 +0300

    add favicon and title

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression()],
   build: {
     outDir: './docs',
   },
